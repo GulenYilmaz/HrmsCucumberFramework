2889d33642d81a93c43cd6fe65d5459f8e7c7256
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-06-03 20:58:26 -0400

    new

---
+++ src/test/java/com/hrms/utils/Constants.java
@@ -0,0 +1,36 @@
+package com.hrms.utils;
+
+public class Constants {
+	
+	
+	
+	
+	public static final String CONFIGURATION_FILEPATH=System.getProperty("user.dir")+"\\src\\test\\resources\\configs\\configuration.properties";
+	
+	public static final String TESTDATA_FILEPATH=System.getProperty("user.dir")+"\\src\\test\\resources\\testdata\\HrmsTestData.xlsx";
+	
+	public static final int IMPLICIT_WAIT_TIME=15;
+	
+	public static final int EXPLICIT_WAIT_TIME=30;
+	
+	public static final String REPORT_FILEPATH=System.getProperty("user.dir")+"\\target\\html-report\\HRMS.html";
+	
+	public static final String SCREENSHOT_FILEPATH=System.getProperty("user.dir")+"\\screenshot\\";
+	
+	
+	
+	
+	public static final String OS_NAME=System.getProperty("os.name");
+	
+	public static final String USER_NAME=System.getProperty("user.name");
+
+	
+	
+	public static void main(String[] args) {
+		System.out.println(TESTDATA_FILEPATH);//C:\Users\gulen\eclipse-workspace\HrmsFramework\src\test\resources\testdata\HrmsTestData.xlsx
+		System.out.println(OS_NAME);//Windows 10
+		System.out.println(USER_NAME);//gulen
+		
+		
+	}
+}
