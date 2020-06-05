722b82ca38732715b19bfd8cfdff8fbf1b0b035f
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-06-05 00:29:38 -0400

    new

--- src/test/java/com/hrms/steps/EmployeeSearchSteps.java
+++ src/test/java/com/hrms/steps/EmployeeSearchSteps.java
@@ -1,10 +1,13 @@
 package com.hrms.steps;
+import org.junit.Assert;
+
 import com.hrms.utils.CommonMethods;
 import com.hrms.utils.ConfigsReader;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
 
 public class EmployeeSearchSteps extends CommonMethods{
 	@Given("user is navigated to HRMS")
@@ -37,9 +40,118 @@ public class EmployeeSearchSteps extends CommonMethods{
 
 	@Then("user see employee information is displayed")
 	public void user_see_employee_information_is_displayed() {
-	   System.out.println("Employee is displayed");
-	   tearDown();
+		String actualEmpID="10079";
+		String expectedEmpId=viewEmp.empIDValidation.getText();
+		Assert.assertEquals("Employee IDs do not Match", actualEmpID, expectedEmpId);
+		wait(3);
+		tearDown();
 	}
+	@When("user enters valid employee name")
+	public void user_enters_valid_employee_name1() {
+		sendText(viewEmp.empName, "Bishnu1");
+	}
+	@Then("user see employee name is displayed")
+	public void user_see_employee_information_is_displayed1() {
+		String actualEmpName="Bishnu1";
+		String expectedEmpName=viewEmp.empNameValidation.getText();
+		Assert.assertEquals("Employee names do not Match", actualEmpName, expectedEmpName);
+		wait(3);
+		tearDown();
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*
+ * Console print
+ * 
+ * Jun 04, 2020 12:46:12 PM cucumber.api.cli.Main run
+WARNING: You are using deprecated Main class. Please use io.cucumber.core.cli.Main
+Jun 04, 2020 12:46:12 PM io.cucumber.core.cli.Main run
+WARNING: By default Cucumber is running in --non-strict mode.
+This default will change to --strict and --non-strict will be removed.
+You can use --strict to suppress this warning
 
+Scenario: Search employee by Id                                    # src/test/resources/features/SearchEmployee.feature:2
+  Given user is navigated to HRMS                                  # null
+  And user logins with valid admin credentials to HRMS application # null
+  And user navigates to EmployeeList page                          # null
+  When user enters valid employee ID                               # null
+  And user clicks on the search button                             # null
+  Then user see employee information is displayed                  # null
+
+Scenario: Search employee by name                                  # src/test/resources/features/SearchEmployee.feature:10
+  Given user is navigated to HRMS                                  # null
+  And user logins with valid admin credentials to HRMS application # null
+  And user navigates to EmployeeList page                          # null
+  When user enters valid employee name                             # null
+  And user clicks on the search button                             # null
+  Then user see employee information is displayed                  # null
+
+Undefined scenarios:
+file:///C:/Users/gulen/eclipse-workspace/HrmsCucumberFramework/src/test/resources/features/SearchEmployee.feature:2# Search employee by Id
+file:///C:/Users/gulen/eclipse-workspace/HrmsCucumberFramework/src/test/resources/features/SearchEmployee.feature:10# Search employee by name
+
+2 Scenarios (2 undefined)
+12 Steps (12 undefined)
+0m0.231s
+
+
+You can implement missing steps with the snippets below:
+
+@Given("user is navigated to HRMS")
+public void user_is_navigated_to_HRMS() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("user logins with valid admin credentials to HRMS application")
+public void user_logins_with_valid_admin_credentials_to_HRMS_application() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Given("user navigates to EmployeeList page")
+public void user_navigates_to_EmployeeList_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("user enters valid employee ID")
+public void user_enters_valid_employee_ID() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("user clicks on the search button")
+public void user_clicks_on_the_search_button() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("user see employee information is displayed")
+public void user_see_employee_information_is_displayed() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("user enters valid employee name")
+public void user_enters_valid_employee_name() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
+
+ * 
+ */
 	
 }
--- src/test/resources/features/SearchEmployee.feature
+++ src/test/resources/features/SearchEmployee.feature
@@ -1,19 +1,17 @@
-Feature: mployee Search
-
-Scenario: Search employee by id
-Given user is navigated to HRMS
-And user is logged in with valid admin credentials
-And user navigate to employee list page
-When user enters valid employee id
-And click search button 
-Then user see employee information is displayed
-
+Feature: Employee Search
+ Scenario: Search employee by Id
+  Given user is navigated to HRMS
+  And user logins with valid admin credentials to HRMS application
+  And user navigates to EmployeeList page
+  When user enters valid employee ID
+  And user clicks on the search button
+  Then user see employee information is displayed
+  
 Scenario: Search employee by name
-Given user is navigated to HRMS
-And user is logged in with valid admin credentials
-And user navigate to employee list page
-When user enters valid employee name and last name
-And click search button 
-Then user see employee information is displayed
-
+  Given user is navigated to HRMS
+  And user logins with valid admin credentials to HRMS application
+  And user navigates to EmployeeList page
+  When user enters valid employee name
+  And user clicks on the search button
+  Then user see employee information is displayed
 
